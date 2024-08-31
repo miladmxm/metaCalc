a2ca88dcb5a6b54219916f428c1f80d5a12c071d
miladmxm <92200884+miladmxm@users.noreply.github.com>
2024-08-31 17:29:14 +0330

    edit manifest

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,38 +8,40 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "prompt",
-      name: "Meta Calculator",
-      short_name: "MetaCalc",
-      theme_color: "#1b1d27",
-      background_color: "#1b1d27",
-      display: "standalone",
-      scope: "/",
-      start_url: "/",
-      orientation: "portrait",
-      icons: [
-        {
-          src: "pwa-64x64.png",
-          sizes: "64x64",
-          type: "image/png",
-        },
-        {
-          src: "pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "any",
-        },
-        {
-          src: "maskable-icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-          purpose: "maskable",
-        },
-      ],
+      manifest: {
+        name: "Meta Calculator",
+        short_name: "MetaCalc",
+        theme_color: "#1b1d27",
+        background_color: "#1b1d27",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        orientation: "portrait",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
 });
