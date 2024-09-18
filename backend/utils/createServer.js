e1b801b0be056622cf5bafc9440f0e34cfdfbd41
miladmxm <92200884+miladmxm@users.noreply.github.com>
2024-09-19 01:27:59 +0330

    Used request pipe for load frontend on vite preview

--- backend/utils/createServer.js
+++ backend/utils/createServer.js
@@ -1,27 +1,33 @@
-import { join } from "path";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { adminRouter, mainRouter, userRouter } from "../routes/router.js";
 import errorHandler from "../middlewares/errorHandler.js";
+import request from "request";
 
 const createServer = () => {
   const app = express();
-  app.use(express.static(join(process.cwd(), "public")));
-  
+
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  app.use(cors({ credentials: true, origin: process.env.FRONTEND, methods: ["GET", "POST", "DELETE", "PUT"] }));
-  
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND,
+      methods: ["GET", "POST", "DELETE", "PUT"],
+    })
+  );
+
   app.use("/user", userRouter);
   app.use("/admin", adminRouter);
   app.use("/api", mainRouter);
   app.use(errorHandler);
 
-  app.use("*", (req, res) => {
-    res.sendFile(join(process.cwd(), "public", "index.html"));
+  app.use("*", (req, res,next) => {
+    const url = process.env.FRONTEND+req.params["0"]
+    req.pipe(request({url,qs:req.query})).pipe(res)
   });
   return app;
 };
