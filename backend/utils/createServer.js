ec27652accac64daea53f16f92cfb5cc0d20c65d
miladmxm <92200884+miladmxm@users.noreply.github.com>
2024-09-19 16:11:33 +0330

    fixed: frontend evviroment url for docker

--- backend/utils/createServer.js
+++ backend/utils/createServer.js
@@ -7,7 +7,6 @@ import request from "request";
 
 const createServer = () => {
   const app = express();
-
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
